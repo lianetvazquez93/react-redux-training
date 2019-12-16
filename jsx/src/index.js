3e9316d509037142acf19c50d07d057bcef0da43
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2019-12-16 16:04:30 +0100

    Section 2 (JSX)

---
+++ jsx/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const buttonText = "Click me";
+  const style = { backgroundColor: "blue", color: "white" };
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={style}>{buttonText}</button>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
