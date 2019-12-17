1aa35c18402c0c1a5d70862b23ebbccdf974dcbf
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2019-12-17 11:34:15 +0100

    Showing custom children

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import AprovalCard from "./AprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        content="Nice blog post"
-      />
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author="Alex"
-        timeAgo="Today at 2:00AM"
-        content="I like the subject"
-      />
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author="Jane"
-        timeAgo="Yesterday at 5:00AM"
-        content="I like the writing"
-      />
+      <AprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Nice blog post"
+        />
+      </AprovalCard>
+      <AprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          content="I like the subject"
+        />
+      </AprovalCard>
+      <AprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Jane"
+          timeAgo="Yesterday at 5:00AM"
+          content="I like the writing"
+        />
+      </AprovalCard>
     </div>
   );
 };
