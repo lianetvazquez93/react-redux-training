3c8fca66ce3d0a7565fb53dc9a801348ccce758b
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2019-12-19 12:18:01 +0100

    Finished section 6

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -13,7 +13,7 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -22,6 +22,10 @@ class App extends React.Component {
     }
 
     return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
