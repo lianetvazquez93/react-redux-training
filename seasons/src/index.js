dff86950681422ae161d2758e65f403c9d7326fd
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2019-12-19 11:54:05 +0100

    Specifying default props

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -21,7 +21,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <Spinner />;
+    return <Spinner message="Please accept location request" />;
   }
 }
 
