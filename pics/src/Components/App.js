946e5891c4b52e3185e1cc9611703bc50749e8d8
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2019-12-20 16:36:10 +0100

    Viewing request results

--- pics/src/Components/App.js
+++ pics/src/Components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID 4b6aafca4d91c444272c8e4650353b98d52adab293104e337704472f26125b48",
+      },
+    });
   }
   render() {
     return (
