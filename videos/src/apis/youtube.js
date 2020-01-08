1961bd2b65013101c0bd1c12c7ef9cce18d1886f
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-01-08 14:58:56 +0100

    Styled list of videos

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from "axios";
+
 const KEY = "AIzaSyDtrbfQSOmzspdZidB5xvL7yslMz_wkiX8";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxReasults: 5,
+    key: KEY,
+  },
+});
