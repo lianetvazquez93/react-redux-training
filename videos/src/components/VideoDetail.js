bcabb5f3bc5af49f9b87199a70ef7660e720a080
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-01-08 15:46:43 +0100

    Displying a video player

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -4,9 +4,14 @@ const VideoDetail = ({ video }) => {
   if (!video) {
     return <div>Loading...</div>;
   }
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return (
     <div className="video-detail">
       <div className="ui segment">
+        <div className="ui embed">
+          <iframe src={videoSrc} title={video.snippet.title} />
+        </div>
         <h4 className="ui header">{video.snippet.title}</h4>
         <p>{video.snippet.description}</p>
       </div>
