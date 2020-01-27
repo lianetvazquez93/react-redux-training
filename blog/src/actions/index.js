b2733ad53ec1cfe50bae726932cd5ca1bd8a1425
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-01-27 17:29:33 +0100

    Action creators in action creators

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,12 +1,25 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async dispatch => {
+  console.log("About to fetch posts");
+  await dispatch(fetchPosts());
+  console.log("Fetched posts!");
+};
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
 
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
+
+/**
 export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
 
 const _fetchUser = _.memoize(async (id, dispatch) => {
@@ -14,3 +27,4 @@ const _fetchUser = _.memoize(async (id, dispatch) => {
 
   dispatch({ type: "FETCH_USER", payload: response.data });
 });
+*/
